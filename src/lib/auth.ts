6396fd2c109450a4483a29d01b7a62cd1caf8203
akikoee <leonmh@outlook.de>
2025-08-08 13:42:32 +0200

    undo engine

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,8 @@
+import { PrismaClient } from "@prisma/client";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { prisma } from "./prisma";
 
+const prisma = new PrismaClient();
 export const auth = betterAuth({
   database: prismaAdapter(prisma, { provider: "postgresql" }),
   socialProviders: {
